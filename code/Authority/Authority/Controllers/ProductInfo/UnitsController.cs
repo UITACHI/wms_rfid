c5d121d2640dc0b8b196edcde9866bc7a5d352bc
wu <981145751@qq.com>
2012-07-04 16:48:25 +0800

    增加产品信息管理页面 Signed-off-by: wu <981145751@qq.com>

---
+++ code/Authority/Authority/Controllers/ProductInfo/UnitsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authority.Controllers.ProductInfo
+{
+    public class UnitsController : Controller
+    {
+        //
+        // GET: /Units/
+
+        public ActionResult Index()
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasAdd = true;
+            ViewBag.hasEdit = true;
+            ViewBag.hasDelete = true;
+            return View();
+        }
+
+    }
+}
