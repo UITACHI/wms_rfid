a4393a5478da83069d7b1dda53bd557b72aeb7d4
zhuyong <846621944@qq.com>
2012-07-06 15:01:53 +0800

    模块改动 Signed-off-by: zhuyong <846621944@qq.com>

---
+++ code/Authority/Authority/Controllers/Wms/Inventory management/distributionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authority.Controllers.Wms.Inventory_management
+{
+    public class distributionController : Controller
+    {
+        //
+        // GET: /distribution/
+
+        public ActionResult Index()
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            return View();
+        }
+
+    }
+}
