bd96aeb2baf959d74071549fc28e579a30353741
zxl <loou@vip.qq.com>
2012-07-06 17:03:19 +0800

    添加分拣，盘点，损益部分页面 Signed-off-by: zxl <loou@vip.qq.com>

---
+++ code/Authority/Authority/Controllers/Wms/StockCheckInfo/CheckBillVerifyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authority.Controllers.Wms.StockCheckInfo
+{
+    public class CheckBillVerifyController : Controller
+    {
+        //
+        // GET: /CheckBillVerify/
+
+        public ActionResult Index()
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasAdd = true;
+            ViewBag.hasEdit = true;
+            ViewBag.hasDelete = true;
+            ViewBag.hasAudit = true;
+            ViewBag.hasAntiTrial = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            return View();
+        }
+
+    }
+}
