4addbfb14857c1fdbbefa69f20baa6d27225b1b7
wu <981145751@qq.com>
2012-07-10 14:33:31 +0800

    增加产品，供应商，品牌，单位，单位系列接口和service Signed-off-by: wu <981145751@qq.com>

---
+++ code/Authority/THOK.Authority.Bll/Service/Wms/SupplierService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.RfidWms.DBModel.Ef.Models.Wms;
+using THOK.Authority.Bll.Interfaces.Wms;
+using Microsoft.Practices.Unity;
+using THOK.Authority.Dal.Interfaces.Wms;
+
+namespace THOK.Authority.Bll.Service.Wms
+{
+    public class SupplierService:ServiceBase<Supplier>,ISupplierService
+    {
+        [Dependency]
+        public ISupplierRepository SupplierRepository { get; set; }
+
+
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+    }
+}
---
+++ code/Authority/THOK.Authority.Bll/Service/Wms/UnitListService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.RfidWms.DBModel.Ef.Models.Wms;
+using THOK.Authority.Bll.Interfaces.Wms;
+using Microsoft.Practices.Unity;
+using THOK.Authority.Dal.Interfaces.Wms;
+
+namespace THOK.Authority.Bll.Service.Wms
+{
+    public class UnitListService:ServiceBase<UnitList>,IUnitService
+    {
+        [Dependency]
+        public IUnitListRepository UnitListRepository { get; set; }
+
+
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+
+    }
+}
