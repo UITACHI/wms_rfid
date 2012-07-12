ae2600ce4912253115707e3591ebc6a1c4fe472d
zhuyong <846621944@qq.com>
2012-07-12 11:23:45 +0800

    产品信息 Signed-off-by: zhuyong <846621944@qq.com>

--- code/Authority/Authority/Controllers/Wms/ProductInfo/ProductController.cs
+++ code/Authority/Authority/Controllers/Wms/ProductInfo/ProductController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Microsoft.Practices.Unity;
+using System.Web.Routing;
+using THOK.WebUtil;
+using THOK.Authority.Bll.Interfaces.Wms;
 namespace Authority.Controllers.ProductInfo
 {
     public class ProductController : Controller
     {
         //
         // GET: /Product/
+        [Dependency]
+        public IProductService ProductService { get; set; }
 
         public ActionResult Index()
         {
@@ -18,6 +23,13 @@ namespace Authority.Controllers.ProductInfo
             ViewBag.hasEdit = true;
             ViewBag.hasDelete = true;
             return View();
+        }
+        public ActionResult Details(int page, int rows, FormCollection collection)
+        {
+            string cityName = collection["CityName"] ?? "";
+
+            var users = ProductService.GetDetails(page, rows, cityName);
+            return Json(users, "text", JsonRequestBehavior.AllowGet);
         }
 
     }
--- code/Authority/THOK.Authority.Bll/Service/Wms/ProductService.cs
+++ code/Authority/THOK.Authority.Bll/Service/Wms/ProductService.cs
@@ -19,5 +19,80 @@ namespace THOK.Authority.Bll.Service.Wms
         {
             get { return this.GetType(); }
         }
+
+        #region IProductService 增，删，改，查等方法
+
+        public object GetDetails(int page, int rows, string CompanyCode)
+        {
+            IQueryable<Product> ProductQuery = ProductRepository.GetQueryable();
+            var product = ProductQuery.Where(c => c.ProductName.Contains(CompanyCode))
+                .OrderBy(c => c.ProductCode).AsEnumerable()
+                .Select(c => new
+                {
+                    c.AbcTypeCode,
+                    c.BarBarcode,
+                    c.BelongRegion,
+                    c.BrandCode,
+                    c.BuyPrice,
+                    c.CostPrice,
+                    c.CustomCode,
+                    c.Description,
+                    c.IsAbnormity,
+                    c.IsActive,
+                    c.IsConfiscate,
+                    c.IsFamous,
+                    c.IsFilterTip,
+                    c.IsMainProduct,
+                    c.IsNew,
+                    c.IsProvinceMainProduct,
+                    c.OneProjectBarcode,
+                    c.PackageBarcode,
+                    c.PackTypeCode,
+                    c.PieceBarcode,
+                    c.PriceLevelCode,
+                    c.ProductCode,
+                    c.ProductName,
+                    c.ProductTypeCode,
+                    c.RetailPrice,
+                    c.ShortCode,
+                    c.StatisticType,
+                    c.SupplierCode,
+                    c.TradePrice,
+                    c.UniformCode,
+                    c.UnitCode,
+                    c.UnitListCode,
+                    c.UpdateTime
+                    //ParentCompanyName = c.ParentCompany.CompanyName,
+                    //c.ParentCompanyID,
+                    //IsActive = c.IsActive == "1" ? "可用" : "不可用",
+                    //UpdateTime = c.UpdateTime.ToString("yyyy-MM-dd hh:mm:ss")
+                });
+            //if (!IsActive.Equals(""))
+            //{
+            //    string bStatus = IsActive == "可用" ? "1" : "0";
+            //    company = companyQuery.Where(c => c.CompanyCode.Contains(CompanyCode) && c.CompanyName.Contains(CompanyName) && c.CompanyType.Contains(CompanyType) && c.IsActive.Contains(bStatus))
+            //    .OrderBy(c => c.CompanyCode).AsEnumerable()
+            //    .Select(c => new
+            //    {
+            //        c.ID,
+            //        c.CompanyCode,
+            //        c.CompanyName,
+            //        c.Description,
+            //        c.CompanyType,
+            //        c.WarehouseCapacity,
+            //        c.WarehouseCount,
+            //        c.WarehouseSpace,
+            //        c.SortingCount,
+            //        ParentCompanyName = c.ParentCompany.CompanyName,
+            //        c.ParentCompanyID,
+            //        IsActive = c.IsActive == "1" ? "可用" : "不可用",
+            //        UpdateTime = c.UpdateTime.ToString("yyyy-MM-dd hh:mm:ss")
+            //    });
+            //}
+            int total = product.Count();
+            product = product.Skip((page - 1) * rows).Take(rows);
+            return new { total, rows = product.ToArray() };
+        }
+        #endregion
     }
 }
