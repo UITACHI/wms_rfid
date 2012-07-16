5db4d4a75040de992a8165e4405a8256154b6016
wu <981145751@qq.com>
2012-07-16 16:46:21 +0800

    添加入库、出库单据表接口实现类 Signed-off-by: wu <981145751@qq.com>

---
+++ code/Authority/THOK.Wms.Bll/Interfaces/IInBillMaster.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace THOK.Wms.Bll.Interfaces
+{
+    public interface IInBillMaster:IService<IInBillMaster>
+    {
+    }
+}
---
+++ code/Authority/THOK.Wms.Bll/Service/InBillMasterService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Wms.DbModel;
+using THOK.Wms.Bll.Interfaces;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Dal.Interfaces;
+
+namespace THOK.Wms.Bll.Service
+{
+    public class InBillMasterService:ServiceBase<InBillMaster>,IInBillMaster
+    {
+    }
+}
