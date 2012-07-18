b3506076817fa760852d9272f8613f636849e605
zhuyong <846621944@qq.com>
2012-07-18 16:36:42 +0800

    配送信息表增加 Signed-off-by: zhuyong <846621944@qq.com>

---
+++ code/Authority/THOK.Wms.DbModel/DeliverLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace THOK.Wms.DbModel
+{
+    public class DeliverLine
+    {
+        public DeliverLine()
+        {
+            this.Customers = new List<Customer>();
+        }
+        public string DeliverLineCode { get; set; }
+        public string CustomCode { get; set; }
+        public string DeliverLineName { get; set; }
+        public string DistCode { get; set; }
+        public int DeliverOrder { get; set; }
+        public string Description { get; set; }
+        public string IsActive { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+
+        public virtual DeliverDist DeliverDist { get; set; }
+        public virtual ICollection<Customer> Customers { get; set; }
+    }
+}
