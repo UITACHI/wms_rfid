3c5c7918f38b76b84d6fbc83915b313aad33cb76
wu <981145751@qq.com>
2012-07-23 09:23:19 +0800

    增加入库明细删除功能 Signed-off-by: wu <981145751@qq.com>

--- code/Authority/Wms/Controllers/Wms/StockIn/StockInBillController.cs
+++ code/Authority/Wms/Controllers/Wms/StockIn/StockInBillController.cs
@@ -144,5 +144,16 @@ namespace Authority.Controllers.Wms.StockIn
             return Json(JsonMessageHelper.getJsonMessage(bResult, msg, null), "text", JsonRequestBehavior.AllowGet);
         }
 
+        //
+        // POST: /InBillDetail/InBillDetailEdit/
+
+        [HttpPost]
+        public ActionResult InBillDetailEdit(InBillDetail inBillDetail)
+        {
+            bool bResult = InBillDetailService.Save(inBillDetail);
+            string msg = bResult ? "修改成功" : "修改失败";
+            return Json(JsonMessageHelper.getJsonMessage(bResult, msg, null), "text", JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
