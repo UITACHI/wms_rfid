7ba55fddd877bdc5d0d626e19fc7ff2d3441a2ee
Endis <369029432@qq.com>
2012-07-25 17:16:25 +0800

    移库单查询界面后台 Signed-off-by: Endis <369029432@qq.com>

---
+++ code/Authority/THOK.Wms.Bll/Service/MoveSearchDetailService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Wms.Bll.Interfaces;
+using THOK.Wms.DbModel;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Dal.Interfaces;
+
+namespace THOK.Wms.Bll.Service
+{
+    public class MoveSearchDetailService : ServiceBase<MoveBillDetail>, IMoveSearchDetailService
+    {
+        [Dependency]
+        public IMoveSearchDetailRepository MoveSearchDetailRepository { get; set; }
+
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+
+        #region IMoveSearchDetailRepository 成员
+
+        public object GetDetails(int page, int rows, string BillNo)
+        {
+            if (BillNo != "" && BillNo != null)
+            {
+                IQueryable<MoveBillDetail> MoveBillDetailQuery = MoveSearchDetailRepository.GetQueryable();
+                var MoveBillDetail = MoveBillDetailQuery.Where(i => i.BillNo.Contains(BillNo)).OrderBy(i => i.BillNo).AsEnumerable().Select(i => new
+                {
+                    i.ID,
+                    i.BillNo,
+                    i.ProductCode,
+                    i.UnitCode,
+                    i.Unit.UnitName,
+                    //i.BillQuantity,
+                    //i.RealQuantity,
+                    //i.Price,
+                    //i.Description 
+                });
+                int total = MoveBillDetail.Count();
+                MoveBillDetail = MoveBillDetail.Skip((page - 1) * rows).Take(rows);
+                return new { total, rows = MoveBillDetail.ToArray() };
+            }
+            return "";
+        }
+        #endregion
+    }
+}
---
+++ code/Authority/THOK.Wms.Dal/Interfaces/IMoveSearchDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Wms.DbModel;
+using THOK.Common.Ef.Interfaces;
+
+namespace THOK.Wms.Dal.Interfaces
+{
+    public interface IMoveSearchDetailRepository : IRepository<MoveBillDetail>
+    {
+
+    }
+}
