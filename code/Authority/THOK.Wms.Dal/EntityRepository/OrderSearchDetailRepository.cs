011f78aca937c55be842c2c224b6b97d060f9034
Endis <369029432@qq.com>
2012-07-26 20:33:21 +0800

    分拣订单查询后台 Signed-off-by: Endis <369029432@qq.com>

---
+++ code/Authority/THOK.Wms.Dal/EntityRepository/OrderSearchDetailRepository.cs
@@ -0,0 +1,11 @@
+﻿using THOK.Wms.Dal.Interfaces;
+using THOK.Wms.DbModel;
+using THOK.Common.Ef.EntityRepository;
+
+namespace THOK.Wms.Dal.EntityRepository
+{
+    public class OrderSearchDetailRepository : RepositoryBase<SortOrderDetail>, IOrderSearchDetailRepository
+    {
+
+    }
+}
--- code/Authority/Wms/Controllers/Wms/ComplexSearch/SortOrderSearchController.cs
+++ code/Authority/Wms/Controllers/Wms/ComplexSearch/SortOrderSearchController.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Bll.Interfaces;
 
 namespace Authority.Controllers.Wms.ComplexSearch
 {
     public class SortOrderSearchController : Controller
     {
+        [Dependency]
+        public ISortOrderSearchService SortOrderSearchService { get; set; }
+        [Dependency]
+        public IOrderSearchDetailService OrderSearchDetailService { get; set; }
+
         //
         // GET: /SortOrderSearch/
 
         public ActionResult Index(string moduleID)
         {
             ViewBag.hasSearch = true;
-            ViewBag.hasEdit = true;
-            ViewBag.hasAdd = true;
             ViewBag.hasHelp = true;
             ViewBag.ModuleID = moduleID;
             return View();
         }
 
+        //
+        // GET: /SortOrderSearch/Details/
+
+        public ActionResult Details(int page, int rows, FormCollection collection)
+        {
+            string OrderID = collection["OrderID"] ?? "";
+            string OrderDate = collection["OrderDate"] ?? "";
+            var SortOrder = SortOrderSearchService.GetDetails(page, rows, OrderID, OrderDate);
+            return Json(SortOrder, "text", JsonRequestBehavior.AllowGet);
+        }
+
+        //
+        // GET: /SortOrderSearch/InfoDetails/
+
+        public ActionResult InfoDetails(int page, int rows, string OrderID)
+        {
+            var SortOrderDetail = OrderSearchDetailService.GetDetails(page, rows, OrderID);
+            return Json(SortOrderDetail, "text", JsonRequestBehavior.AllowGet);
+        }
     }
 }
