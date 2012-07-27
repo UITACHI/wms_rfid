246fe6de6dca358299ded9ed1f0afe317a2be5ce
zxl <loou@vip.qq.com>
2012-07-27 19:13:38 +0800

    添加分拣主表和细表查询类和接口 Signed-off-by: zxl <loou@vip.qq.com>

---
+++ code/Authority/THOK.Wms.Bll/Service/SortOrder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Wms.DbModel;
+using THOK.Wms.Bll.Interfaces;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Dal.Interfaces;
+
+namespace THOK.Wms.Bll.Service
+{
+    public class SortOrderService : ServiceBase<SortOrder>, ISortOrderService
+    {
+        [Dependency]
+        public ISortOrderRepository SortOrderRepository { get; set; }
+        protected override Type LogPrefix
+        {
+            get { return this.GetType(); }
+        }
+
+        #region ISortOrderService 成员
+
+        public object GetDetails(int page, int rows, string OrderID, string orderDate, string CustomerCode, string CustomerName)
+        {
+            IQueryable<SortOrder> sortOrderQuery = SortOrderRepository.GetQueryable();
+            var sortOrder = sortOrderQuery.Where(s => s.OrderID.Contains(OrderID) && s.CustomerCode.Contains(CustomerCode) && s.CustomerName.Contains(CustomerName));
+            if (!orderDate.Equals(string.Empty) || orderDate!=null)
+            {
+                sortOrder = sortOrder.Where(i => i.OrderDate == orderDate);
+            }
+            var temp = sortOrder.AsEnumerable().OrderBy(t => t.OrderID).Select(s => new
+            {
+                s.OrderID,
+                s.CompanyCode,
+                s.SaleRegionCode,
+                s.OrderDate,
+                s.OrderType,
+                s.CustomerCode,
+                s.CustomerName,
+                s.QuantitySum,
+                s.AmountSum,
+                s.DetailNum,
+                s.DeliverOrder,
+                s.DeliverDate,
+                IsActive = s.IsActive == "1" ? "可用" : "不可用",
+                UpdateTime = s.UpdateTime.ToString("yyyy-MM-dd HH:mm:ss"),
+                s.Description
+            });
+
+            int total = temp.Count();
+            temp = temp.Skip((page - 1) * rows).Take(rows);
+            return new { total, rows = temp.ToArray() };
+        }
+
+        #endregion
+    }
+}
--- code/Authority/Wms/Controllers/Wms/SortingInfo/SortingOrderController.cs
+++ code/Authority/Wms/Controllers/Wms/SortingInfo/SortingOrderController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using THOK.Wms.Bll.Interfaces;
 
 namespace Authority.Controllers.Wms.SortingInfo
 {
     public class SortingOrderController : Controller
     {
+        [Dependency]
+        public ISortOrderService SortOrderService { get; set; }
+        [Dependency]
+        public ISortOrderDetailService SortOrderDetailService { get; set; }
         //
         // GET: /SortingOrder/
-
         public ActionResult Index(string moduleID)
         {
             ViewBag.hasSearch = true;
@@ -22,5 +27,24 @@ namespace Authority.Controllers.Wms.SortingInfo
             return View();
         }
 
+        //查询主单
+        // GET: /SortingOrder/Details/
+        public ActionResult Details(int page, int rows, FormCollection collection)
+        {
+            string OrderID = collection["OrderID"] ?? "";
+            string orderDate = collection["orderDate"] ?? "";
+            string CustomerCode = collection["CustomerCode"] ?? "";
+            string CustomerName = collection["CustomerName"] ?? "";
+            var sortOrder = SortOrderService.GetDetails(page, rows, OrderID, orderDate, CustomerCode, CustomerName);
+            return Json(sortOrder, "text", JsonRequestBehavior.AllowGet);
+        }
+
+        //查询细单
+        // GET: /SortingOrder/CheckBillDetails/
+        public ActionResult sortOrderDetails(int page, int rows, string OrderID)
+        {
+            var SortOrderDetail = SortOrderDetailService.GetDetails(page, rows, OrderID);
+            return Json(SortOrderDetail, "text", JsonRequestBehavior.AllowGet);
+        }
     }
 }
