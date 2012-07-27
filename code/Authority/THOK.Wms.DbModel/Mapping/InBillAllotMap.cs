c6d1a7c2fab13378f7a07458013fdaab079891c6
zengys <ynnuyonw@126.com>
2012-07-27 20:31:46 +0800

    Add-Migration modify_inBillAllot_UnitCode Add-Migration modify_outBillAllot_UnitCode

--- code/Authority/THOK.Wms.DbModel/Mapping/InBillAllotMap.cs
+++ code/Authority/THOK.Wms.DbModel/Mapping/InBillAllotMap.cs
@@ -61,6 +61,7 @@ namespace THOK.Wms.DbModel.Mapping
             this.Property(t => t.ProductCode).HasColumnName(ColumnMap.Value.To("ProductCode"));
             this.Property(t => t.CellCode).HasColumnName(ColumnMap.Value.To("CellCode"));
             this.Property(t => t.StorageCode).HasColumnName(ColumnMap.Value.To("StorageCode"));
+            this.Property(t => t.UnitCode).HasColumnName(ColumnMap.Value.To("UnitCode"));
             this.Property(t => t.AllotQuantity).HasColumnName(ColumnMap.Value.To("AllotQuantity"));
             this.Property(t => t.RealQuantity).HasColumnName(ColumnMap.Value.To("RealQuantity"));
             this.Property(t => t.OperatePersonID).HasColumnName(ColumnMap.Value.To("OperatePersonID"));
