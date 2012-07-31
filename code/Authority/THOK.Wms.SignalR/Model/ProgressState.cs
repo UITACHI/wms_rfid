b6317d88cad51ba51254b033d38b23e16036176f
zxl <loou@vip.qq.com>
2012-07-31 17:38:21 +0800

    Signed-off-by: zxl <loou@vip.qq.com>

---
+++ code/Authority/THOK.Wms.SignalR/Model/ProgressState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace THOK.Wms.SignalR.Model
+{
+    public enum StateType
+    {
+        Start = 0,
+        Processing = 1,
+        Prompt = 2,
+        Warning = 3,
+        Error = 4,
+        Info = 5,
+        Question = 6,
+        Confirm = 7,
+        Complete = 8
+    }
+
+    public class ProgressState
+    {
+        public StateType State { get; set; }
+        public IList<string> Messages { get; set; }
+        public IList<string> Errors { get; set; }
+        public string TotalProgressName { get; set; }
+        public int TotalProgressValue { get; set; }
+        public string CurrentProgressName { get; set; }
+        public int CurrentProgressValue { get; set; }
+    }
+}
---
+++ code/Authority/Wms/Controllers/Wms/Inventory/CurrentStockController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Authority.Controllers.Wms.Inventory
+{
+    public class CurrentStockController : Controller
+    {
+        //
+        // GET: /CurrentStock/
+
+        public ActionResult Index(string moduleID)
+        {
+            ViewBag.hasSearch = true;
+            ViewBag.hasPrint = true;
+            ViewBag.hasHelp = true;
+            ViewBag.ModuleID = moduleID;
+            return View();
+        }
+    }
+}
