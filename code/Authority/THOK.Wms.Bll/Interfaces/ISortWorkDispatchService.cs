3d68683697bc8f4da4c1d1b8fdfbe2c678bc5ed4
zxl <loou@vip.qq.com>
2012-08-01 11:34:18 +0800

    添加分拣调度作业表接口 Signed-off-by: zxl <loou@vip.qq.com>

---
+++ code/Authority/THOK.Wms.Bll/Interfaces/ISortWorkDispatchService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Wms.DbModel;
+
+namespace THOK.Wms.Bll.Interfaces
+{
+    public interface ISortWorkDispatchService : IService<SortWorkDispatch>
+    {
+    }
+}
