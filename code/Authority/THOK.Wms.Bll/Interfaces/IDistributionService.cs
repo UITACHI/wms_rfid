6eec9b550fc140e52a909ad94b709c44d3cd50b6
Endis <369029432@qq.com>
2012-08-02 17:20:09 +0800

    库存分布查询 Signed-off-by: Endis <369029432@qq.com>

---
+++ code/Authority/THOK.Wms.Bll/Interfaces/IDistributionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using THOK.Wms.DbModel;
+
+namespace THOK.Wms.Bll.Interfaces
+{
+    public interface IDistributionService : IService<Storage>
+    {
+        object GetCellDetails(int page, int rows, string productCode, string ware, string area);
+    }
+}
