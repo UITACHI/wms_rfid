45e1ed7b67b3a49903d8a8d389b48f8fe4805160
zengys <ynnuyonw@126.com>
2012-08-12 17:43:32 +0800

    修改数据结构文档 ；

--- code/Authority/THOK.Wms.DbModel/MoveBillMaster.cs
+++ code/Authority/THOK.Wms.DbModel/MoveBillMaster.cs
@@ -14,6 +14,7 @@ namespace THOK.Wms.DbModel
         public string BillNo { get; set; }
         public DateTime BillDate { get; set; }
         public string BillTypeCode { get; set; }
+        public string Origin { get; set; }
         public string WarehouseCode { get; set; }
         public Guid OperatePersonID { get; set; }
         public string Status { get; set; }
